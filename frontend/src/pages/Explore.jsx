5d671f135bc4770a09d59bbabc9dbf86c48b49c4
meharsurati25 <meharsurati2504@gmail.com>
2026-07-09 11:25:19 +0530

    removed changes of explore page

--- frontend/src/pages/Explore.jsx
+++ frontend/src/pages/Explore.jsx
@@ -1,31 +1,9 @@
-import { useState } from "react";
-
-import hero from "../components/explore_complonents/hero";
-import dinosaurList from "../components/explore_complonents/dinosaurList";
-
-
-function Explore() { 
-    const [search, setSearch] = useState("");
-    const [diet, setDiet] = useState("");
-    const [period, setPeriod] = useState("");   
-    return ( 
-    <>
-        <hero
-            search={search}
-            setSearch={setSearch}
-            diet={diet}
-            setDiet={setDiet}
-            period={period}
-            setPeriod={setPeriod}
-        />
-        <dinosaurList
-            search={search}
-            diet={diet}
-            period={period}
-        />
-    </>
-        
-    ); 
+function Explore() {
+    return (
+        <>
+            <h1>Explore PAGE</h1>
+        </>
+    );
 }
 
 export default Explore;
