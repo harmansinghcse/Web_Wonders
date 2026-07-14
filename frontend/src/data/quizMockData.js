b519f52775a7856085d67375ad28056a4a7dacde
meharsurati25 <meharsurati2504@gmail.com>
2026-07-14 15:43:28 +0530

    Setup Quiz dashboard architecture

---
+++ frontend/src/data/quizMockData.js
@@ -0,0 +1,15 @@
+// TODO (Backend)
+// Replace this mock data with API response from:
+// GET /quiz/dashboard
+
+export const dashboardData = {
+    user: {
+        name: "Explorer",
+        streak: 12,
+        dnaPoints: 1250,
+        accuracy: 82,
+        solved: 87,
+    },
+    dailyChallenge: {},
+    topics : [],
+};
