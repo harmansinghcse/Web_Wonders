8422582be7f804e7853d19c9f466db8b358dc046
hetsavani567 <u25cs026@coed.svnit.ac.in>
2026-07-24 21:09:40 +0530

    applied custom cursor in map secion

---
+++ frontend/src/components/map/Cursor.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from "react";
+
+export default function JurassicCursor() {
+    const cursorRef = useRef(null);
+
+    useEffect(() => {
+        document.body.style.cursor = "none";
+
+        const moveCursor = (e) => {
+            if (cursorRef.current) {
+                cursorRef.current.style.left = `${e.clientX}px`;
+                cursorRef.current.style.top = `${e.clientY}px`;
+            }
+        };
+
+        window.addEventListener("mousemove", moveCursor);
+
+        return () => {
+            document.body.style.cursor = "auto";
+            window.removeEventListener("mousemove", moveCursor);
+        };
+    }, []);
+
+    return (
+        <div
+            ref={cursorRef}
+            className="fixed pointer-events-none z-99999 -translate-x-1/2 -translate-y-1/2 transition-transform duration-100"
+        >
+        <img
+            src="/mapcursor.png"
+            alt="cursor"
+            className="w-10 h-10 object-contain"
+        />
+        </div>
+    );
+}
